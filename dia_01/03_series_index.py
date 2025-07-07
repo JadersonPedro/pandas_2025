272113394042486f1ec5a6e2d0df0c8d9713b4c2
jaderson <jadersonengbio@gmail.com>
2025-07-06 23:04:31 -0300

    Curso de Pandas do Teo Calvo

---
+++ dia_01/03_series_index.py
@@ -0,0 +1,29 @@
+#%%
+import pandas as pd
+
+idades = [32, 38, 30, 30, 31, 
+          35, 25, 29, 31, 37, 
+          27, 23, 36, 33, 39,
+]
+
+series_idades = pd.Series(idades)
+series_idades
+
+
+#%%
+
+idades[0]
+series_idades[0]
+
+#%%
+
+series_idades = series_idades.sort_values()
+series_idades
+
+#%%
+
+series_idades.iloc[0]
+
+#%%
+
+series_idades.iloc[-1]
